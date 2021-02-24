29c94f6f67526b8f2278e4457a6e2f5e34a9582b
Rullow <rullowgl@gmail.com>
2021-02-24 21:04:14 +0100

    Removed list_gen()

--- main.py
+++ main.py
@@ -1,30 +1,8 @@
 import solution
-import random
-
-
-def list_gen():
-    length = random.randint(1, 10)
-    array = []
-    product_positive = 1
-    product_negative = 1
-    highest_negative = -2**31
-    for i in range(0, length):
-        array.append(random.randint(-10, 10))
-        if array[i] > 0:
-            product_positive *= array[i]
-        elif array[i] < 0:
-            product_negative *= array[i]
-            if array[i] > highest_negative:
-                highest_negative = array[i]
-    print("Product positive: " + str(product_positive))
-    print("Product negative: " + str(product_negative))
-    print("Highest negative: " + str(highest_negative))
-    print(array)
-    return array
 
 
 def main():
-    print(solution.solution(list_gen()))
+    print(solution.solution([2, -3, 1, 0, -5]))
 
 
 if __name__ == '__main__':
